557c54eb2ac07944645c231aaace9fb5dda21f66
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-03-08 01:02:24 -0500

    Added project

--- src/Components/Projects/projects.component.jsx
+++ src/Components/Projects/projects.component.jsx
@@ -3,7 +3,8 @@ import './projects.styles.sass';
 import { Container } from 'react-bootstrap';
 import Title from '../Title/title.component';
 import Project from "../Project/project.component";
-import FriendMatch from '../../assets/images/projects/friendmatch.gif'
+import FriendMatch from '../../assets/images/projects/friendMatch.png'
+import EatTheBurger from '../../assets/images/projects/eatTheBurger.png'
 
 const Projects = () => {
   return (
@@ -11,7 +12,8 @@ const Projects = () => {
       <Container>
         <div className="project-wrapper">
           <Title title='Projects'/>
-          <Project title='FriendMatch' info={`Technologies Used: HTML, CSS, JavaScript, Bootstrap, ExpressJS`} projectImage={FriendMatch} repo='http://github.com/francofle/friendMatch.git' url='https://portfolio-friend-match.herokuapp.com/' />
+          <Project title='Eat The Burger' info={`Technologies Used: HTML, CSS, JavaScript, Handlebars, ExpressJS, MySQL.`} projectImage={EatTheBurger} repo='http://github.com/francofle/eat-the-burger' url='https://eat-da-burger-hdlbrs-xprs-sql.herokuapp.com/' />
+          <Project title='FriendMatch' info={`Technologies Used: HTML, CSS, JavaScript, Bootstrap, ExpressJS`} projectImage={FriendMatch} repo='http://github.com/francofle/friendMatch' url='https://portfolio-friend-match.herokuapp.com/' />
           <hr/>
         </div>
       </Container>
