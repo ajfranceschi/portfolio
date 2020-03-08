a5c230869ffb0f5704b4b92aa04d4a5bdfc09874
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-03-08 03:38:51 -0400

    Added React-Memory-Game

--- src/Components/Projects/projects.component.jsx
+++ src/Components/Projects/projects.component.jsx
@@ -5,6 +5,7 @@ import Title from '../Title/title.component';
 import Project from "../Project/project.component";
 import FriendMatch from '../../assets/images/projects/friendMatch.png'
 import EatTheBurger from '../../assets/images/projects/eatTheBurger.png'
+import ReactMemoryGame from '../../assets/images/projects/reactMemoryGame.png'
 
 const Projects = () => {
   return (
@@ -12,8 +13,9 @@ const Projects = () => {
       <Container>
         <div className="project-wrapper">
           <Title title='Projects'/>
-          <Project title='Eat The Burger' info={`Technologies Used: HTML, CSS, JavaScript, Handlebars, ExpressJS, MySQL.`} projectImage={EatTheBurger} repo='http://github.com/francofle/eat-the-burger' url='https://eat-da-burger-hdlbrs-xprs-sql.herokuapp.com/' />
-          <Project title='FriendMatch' info={`Technologies Used: HTML, CSS, JavaScript, Bootstrap, ExpressJS`} projectImage={FriendMatch} repo='http://github.com/francofle/friendMatch' url='https://portfolio-friend-match.herokuapp.com/' />
+          <Project title='Eat The Burger' info={`Technologies Used: MySQL, ExpressJS, Handlebars, JavaScript, HTML, CSS.`} projectImage={EatTheBurger} repo='http://github.com/francofle/eat-the-burger' url='https://eat-da-burger-hdlbrs-xprs-sql.herokuapp.com/' />
+          <Project title='React Memory Game' info={`Technologies Used: React, JavaScript, HTML, CSS`} projectImage={ReactMemoryGame} repo='http://github.com/francofle/react-memory-game' url='https://ajf-react-memory-game.herokuapp.com/' />
+          <Project title='FriendMatch' info={`Technologies Used: ExpressJS, JavaScript, HTML, CSS,  Bootstrap.`} projectImage={FriendMatch} repo='http://github.com/francofle/friendMatch' url='https://portfolio-friend-match.herokuapp.com/' />
           <hr/>
         </div>
       </Container>
